6820eda5598b0d269598e3c966128750f0df983c
Fortivera <ant97str@gmail.com>
2024-01-06 14:17:11 -0500

    add endpoint

--- .history/src/app.controller_20240106141212.ts
+++ .history/src/app.controller_20240106141212.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
 
-@Controller('getHello')
+@Controller('myGPT')
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('message')
   getHello(): string {
     return this.appService.getHello()
   }
